256bc8a14650c3031892202c049ec2c38f416dd0
Tavo Soto <gsoto@itx.com>
2023-03-15 12:42:29 -0500

    Expanded examples of block scoping using ES6 let variable type definition

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
 /* ==========================================================================
     Javascript: The Advanced Concepts (Javascript Foundation II ep. 43)
     
-    Function vs Block Scope
+    Block Scope
 ========================================================================== */
 
-/* =================================================
-    Function Scope (Classic JS)
-    Normally in JavaScript, variables defined within
-    a function's scope can not exist outside of the
-    function.
-================================================= */
-function a() {
-    var secret = '12345';
-}
-  
-console.log(secret);
-  
-/* =================================================
-    Block Scope (Classic JS)
-================================================= */
-// Normally in JavaScript, variables defined within
-// a code bock will exist
-// outside of the block.
-if (5 > 4) {
-    var secret = '12345';
+// Note: Classic JavaScript's block scope for a loop.
+//       The final print statement will log the final
+//       int value after the loop was broken out of.
+console.log('------------- Example 1 -------------');
+console.log('=====================================');
+function loop() {
+    for(var i = 0; i < 5; i++) {
+        console.log('Inner Loop: ' + i);
+    }
+
+    console.log('final: ' + i);
 }
 
-console.log(secret);
+loop();
+console.log('=====================================');
 
-// With ES6, if you use let or const... the defined
-// variable will be scoped inside of the code block.
+// NOTE: ES6 Block based scoping using let via the 
+//       variable definition of the index variable
+//       'i'.
+console.log(' ');
+console.log('------------- Example 2 -------------');
+console.log('=====================================');
+function loop() {
+    for(let i = 0; i < 5; i++) {
+        console.log('Inner Loop: ' + i);
+    }
+
+    // Note: This log will now raise an error because
+    //       the usage of 'let i' in the loop scopes
+    //       the 'i' variable to the loop's code block
+    //       ONLY. It will now not exist outside of
+    //       that code block! 
+    // console.log('final: ' + i);
+}
+
+loop();
+console.log('=====================================');
