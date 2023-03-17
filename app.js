d97b43aa6d72a936c919421dca0dd8a69cfd12f2
Tavo Soto <gsoto@itx.com>
2023-03-17 12:47:58 -0500

    Example usages of this in javascript

--- app.js
+++ app.js
@@ -1,80 +1,100 @@
 /* ==========================================================================
     Javascript: The Advanced Concepts (Javascript Foundation II ep. 43)
     
-    IIFE (immediately involked function expression)
+    What is 'this' keyword?
+        - 'this' is the object that the function is a property of. Thats
+          it.
+    
+    NOTE: By default, 'this' in the browser will be the browser's 'window'
+          object.
 ========================================================================== */
-
 console.log(
     '------------- Example 1 -------------\n' +
-    '====================================='
+    '=====================================\n' +
+    'Global this var type: ' + typeof this + '\n' +
+    '-------------------------------------'
 );
-// I use to recognize this as a no conflict wrapper.
-// All library code will exist in its own scope to prevent
-// naming conflicts.
-(function() { // This isn't a function declaration, but a functional expression. ()();
-    // This inner function is an anonymous function definition. function(){}
-    console.log('Hello from my IIFE!');
-})();
-
-// What is the benefit? 
-//     - All variables defined in an IIFE will be scoped to that IIFE
+function a() {
+    console.log(
+        'The \'this\' within the function a() is the \n' +
+        'same: ' + this + '\n\n' +
+        'That is because \'this\' is the OBJECT that the \n' +
+        'FUNCTION a() is a property of. Which means the \n' +
+        'GLOBAL context of \'this\' that ALL global functions \n' +
+        'and variables are written to!!!'
+    );
+}
+a();
 console.log(
     '=====================================\n\n' +
     '------------- Example 2 -------------\n' +
-    '=====================================\n' +
-    'We will declare a new variable z and\n' + 
-    'function a().\n'
+    'How can we prevent this global assignment \n' +
+    'leakage of \'this\' context into our function? \n' +
+    '    - by invoking \'use scrict\' \n' +
+    '=====================================\n' 
 );
-var z = 1;
-function a() {
-    return 5;
+function b() {
+    'use strict'
+    console.log(
+        'What is \'this\' while invoking \n' +
+        '\'use strict\': ' + this
+    );
 }
+b();
+
 console.log(
-    'Var z: ' + z + '\n' +
-    'Function a: ' + a() + '\n' +
-    '-------------------------------------\n' +
-    'We will re-declare a new variable z and function \n' + 
-    'a(). Notice that our old variable and funnction \n' +
-    'will now get replaced! \n'
-);
-var z = 1000;
-function a() {
-    return 'hahahahahah';
-}
-console.log(
-    'Var z: ' + z + '\n' +
-    'Function a: ' + a() + '\n\n' +
-    'Now, this is a problem because we have illustrated a \n' +
-    'namespace conflict. In the wild, imagine one developer \n' +
-    'overwriting another developer\'s function by re-using \n'  +
-    'the same variable or function name unintentionally. This \n' +
-    'would break code in places the second developer wasn\'t \n' + 
-    'working on. \n' +
     '=====================================\n\n' +
     '------------- Example 3 -------------\n' +
-    '=====================================\n' +
-    'How can we fix this? \n'
+    'Practical usage of \'this\' in javascript. \n' +
+    '=====================================' 
 );
-
-var script1 = (function() {
-    function a() {
-        return 5;
+const obj = {
+    name: 'Billy',
+    sing() {
+        return 'lalala ' + this.name;
     }
-
-    return {a: a()};
-})();
-
-function a() {
-    return 'hahhaha';
+}
+console.log(
+    obj.sing() + '\n' +
+    '=====================================\n\n' +
+    '------------- Example 4 -------------\n' +
+    'Practical example II usage of \'this\' \n' + 
+    'in javascript. \n' +
+    '=====================================' 
+);
+const obj2 = {
+    name: 'Billy',
+    sing() {
+        return 'lalala ' + this.name;
+    },
+    singAgain() {
+        return this.sing() + '!';
+    }
+}
+console.log(
+    obj2.singAgain() + '\n' +
+    '=====================================\n\n' +
+    '------------- Example 5 -------------\n' +
+    'Practical example III usage of \'this\' \n' + 
+    'in javascript. \n' +
+    '=====================================' 
+);
+// The value of this within shared functions is:
+//     1: Gives methods access to their object.
+//     2: Executes same code for multiple objects.Y
+function importantPersion() {
+    console.log(this.name + '!');
 }
 
-console.log(
-    'Global function a(): ' + a() + '\n' +
-    'IIFE scoped function a(): ' + script1.a + '\n\n' +
-    'Note that scoping the a function into the script1 \n' + 
-    'variable only pollutes the global namespace once but \n' +
-    'will encapsulate all of the functions within it \n' +
-    'via the returned object. jQuery use to do this \n' +
-    'way back in the day. This example is an introduction \n' +
-    'to a future lesson called "Modules." \n'
-);
+const object1 = {
+    name: 'Cassy',
+    importantPersion: importantPersion
+}
+
+const object2 = {
+    name: 'Jacob',
+    importantPersion: importantPersion
+}
+object1.importantPersion();
+object2.importantPersion();
+console.log('=====================================');
